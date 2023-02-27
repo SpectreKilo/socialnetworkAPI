8635be65d265b59c1245fee9ebfbe523355739c6
SpectreKilo <johnho8788@gmail.com>
2023-02-27 11:00:46 -0500

    finished routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,29 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    newFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+
+
+router.route('/')
+    .get(getUsers)
+    .post(createUser) 
+
+
+router
+    .route('/:userId')
+    .get(getOneUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+
+router.route('/:userId/friends/:friendId')
+    .post(newFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
